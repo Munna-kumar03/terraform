97e5891352228fd3296ef2a25f02f62e4e3fb709
Munna-kumar03 <munnakr4devops@gmail.com>
2024-05-22 09:22:43 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+  default     = terraform-s3-bucket-for-user
+}
+
+variable "folder_name" {
+  description = "The folder name in the S3 bucket"
+  type        = string
+  default     = path/to/folder
+}
+
+variable "user_name" {
+  description = "The name of the IAM user"
+  type        = string
+  default     = user-name
+}
+
+variable "user_arn" {
+  description = "The ARN of the IAM user"
+  type        = string
+}
